803e44aa770d07267e9c5cea2e2bf0b30c7bff62
Yifan Yang <yifan.yang@hotmail.ca>
2023-11-22 17:06:15 -0500

    [Nav] New music section to navigation bar
    Added a new music section that will play custom music and a
    signature that will act as the site logo. Clicking upon the logo
    will navigate the user back to the 'Home' tab.

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,8 +1,10 @@
 "use client"
 
 import Link from 'next/link';
+import Image from 'next/image'
+import howl from '@/resources/Howl.jpeg'
+import signature from '@/resources/Signature.png'
 import { usePathname } from 'next/navigation';
-import { useState } from 'react';
 import styles from './navigation.module.css'
 
 export default function Navigation() {
@@ -29,20 +31,23 @@ export default function Navigation() {
             'path' : '/contact'
         },
     ];
+    const howlStyle = {
+        width: '100%',
+        height: 'auto',
+        borderRadius: 'var(--border-radius)',
+    }
 
     function getTabs() {
         return (
-            <ul className={styles.list}>
+            <ul className={styles.tab_list}>
                 {tabs.map((tab, index) => (
                     <li
                         className={
                             styles['tab'] + " " +
-                            `${pathname == tab.path ? styles['tab-active'] : ''}`}
+                            `${pathname == tab.path ? styles['tab_active'] : ''}`}
                         key={index}
                     >
-                        <Link
-                            href={tab.path}
-                        >
+                        <Link href={tab.path}>
                             <div className={styles.title}>{tab.title}</div>
                             <div>{tab.desc}</div>
                         </Link>
@@ -54,8 +59,36 @@ export default function Navigation() {
 
     return (
         <div className={styles.navigation}>
-            {getTabs()}
+            <div className={styles.navigate}>
+                <Image
+                    src={signature}
+                    alt='signature logo'
+                    height={75}
+                    width={75}
+                />
+                {getTabs()}
+            </div>
+
             <div className={styles.music}>
+                <div className={styles.image}>
+                    <Image
+                        src={howl}
+                        alt='Howl and Sophie'
+                        style={howlStyle}
+                        priority={true}
+                    />
+                </div>
+                <div>
+                    <div className={styles.marquee}>
+                        <ul className={styles.marquee_content}>
+                            <li>Merry-Go-Around of Fate</li>
+                        </ul>
+                        <ul aria-hidden="true" className={styles.marquee_content}>
+                            <li>Merry-Go-Around of Fate</li>
+                        </ul>
+                    </div>
+                    <div className={styles.composer}>Joe Hisaishi</div>
+                </div>
             </div>
         </div>
     )
